f6d03920fa761a450ea3701c1f2de447c532df4a
razerx100 <saikatmandal8@gmail.com>
2024-01-27 11:54:30 +0000

    Add test for VkAllocator

--- test/src/AllocatorTest.cc
+++ test/src/AllocatorTest.cc
@@ -2,7 +2,6 @@
 #include <memory>
 
 #include <VKInstanceManager.hpp>
-#include <SurfaceManagerWin32.hpp>
 #include <VkDeviceManager.hpp>
 #include <VkResources.hpp>
 
@@ -13,7 +12,7 @@ namespace Constants
 	constexpr const char* appName  = "TerraTest";
 }
 
-/*class AllocatorTest : public ::testing::Test
+class AllocatorTest : public ::testing::Test
 {
 protected:
 	static void SetUpTestSuite();
@@ -22,10 +21,6 @@ protected:
 protected:
 	inline static std::unique_ptr<VkInstanceManager>   s_instanceManager;
 	inline static std::unique_ptr<VkDeviceManager>     s_deviceManager;
-	inline static std::unique_ptr<SurfaceManagerWin32> s_surface;
-	inline static SimpleWindow                         s_window{
-		Constants::width, Constants::height, Constants::appName
-	};
 };
 
 void AllocatorTest::SetUpTestSuite()
@@ -34,18 +29,13 @@ void AllocatorTest::SetUpTestSuite()
 	s_instanceManager->CreateInstance();
 	VkInstance vkInstance = s_instanceManager->GetVKInstance();
 
-	s_surface = std::make_unique<SurfaceManagerWin32>(
-		vkInstance, s_window.GetWindowHandle(), s_window.GetModuleInstance()
-	);
-
 	s_deviceManager = std::make_unique<VkDeviceManager>();
-	s_deviceManager->FindPhysicalDevice(vkInstance, s_surface->GetSurface()).CreateLogicalDevice(false);
+	s_deviceManager->FindPhysicalDevice(vkInstance, VK_NULL_HANDLE).CreateLogicalDevice(false);
 }
 
 void AllocatorTest::TearDownTestSuite()
 {
 	s_deviceManager.reset();
-	s_surface.reset();
 	s_instanceManager.reset();
 }
 
@@ -56,10 +46,12 @@ TEST_F(AllocatorTest, VkAllocatorTest)
 
 	MemoryManager memoryManager{ physicalDevice, logicalDevice, 2_MB, 200_KB };
 
-	Buffer buffer{ logicalDevice, memoryManager, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT };
-	buffer.Create(logicalDevice, 1_KB, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, {});
+	{
+		Buffer buffer{ logicalDevice, memoryManager, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT };
+		buffer.Create(logicalDevice, 1_KB, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, {});
 
-	EXPECT_NE(buffer.Get(), VK_NULL_HANDLE) << "Buffer wasn't initialised";
-	EXPECT_EQ(buffer.CPUHandle(), nullptr) << "CPU Pointer isn't null.";
-	EXPECT_EQ(buffer.Size(), 1_KB) << "BufferSize doesn't match.";
-}*/
+		EXPECT_NE(buffer.Get(), VK_NULL_HANDLE) << "Buffer wasn't initialised";
+		EXPECT_EQ(buffer.CPUHandle(), nullptr) << "CPU Pointer isn't null.";
+		EXPECT_EQ(buffer.Size(), 1_KB) << "BufferSize doesn't match.";
+	}
+}
