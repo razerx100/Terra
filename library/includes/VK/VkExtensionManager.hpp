678336958527133f35eaa816a4c1d1232a74052c
razerx100 <saikatmandal8@gmail.com>
2024-01-28 11:26:15 +0000

    Add VkExtDescriptorBuffer

--- library/includes/VK/VkExtensionManager.hpp
+++ library/includes/VK/VkExtensionManager.hpp
@@ -10,6 +10,7 @@ enum class DeviceExtension
 	VkExtMeshShader,
 	VkKhrSwapchain,
 	VkExtMemoryBudget,
+	VkExtDescriptorBuffer,
 	None
 };
 
@@ -42,6 +43,7 @@ private:
 
 private:
 	static void PopulateVkExtMeshShader(VkDevice device) noexcept;
+	static void PopulateVkExtDescriptorBuffer(VkDevice device) noexcept;
 
 private:
 	std::bitset<static_cast<size_t>(DeviceExtension::None)> m_extensions;
@@ -82,6 +84,108 @@ namespace VkDeviceExtension
 		inline static PFN_vkCmdDrawMeshTasksEXT s_vkCmdDrawMeshTasksEXT                           = nullptr;
 		inline static PFN_vkCmdDrawMeshTasksIndirectCountEXT s_vkCmdDrawMeshTasksIndirectCountEXT = nullptr;
 		inline static PFN_vkCmdDrawMeshTasksIndirectEXT s_vkCmdDrawMeshTasksIndirectEXT           = nullptr;
+	};
+
+	class VkExtDescriptorBuffer
+	{
+		friend class VkDeviceExtensionManager;
+	public:
+		static void vkCmdBindDescriptorBufferEmbeddedSamplersEXT(
+			VkCommandBuffer commandBuffer, VkPipelineBindPoint pipelineBindPoint,
+			VkPipelineLayout layout, std::uint32_t set
+		)
+		{
+			s_vkCmdBindDescriptorBufferEmbeddedSamplersEXT(
+				commandBuffer, pipelineBindPoint, layout, set
+			);
+		}
+		static void vkCmdBindDescriptorBuffersEXT(
+			VkCommandBuffer commandBuffer, std::uint32_t bufferCount,
+			const VkDescriptorBufferBindingInfoEXT* pBindingInfos
+		)
+		{
+			s_vkCmdBindDescriptorBuffersEXT(commandBuffer, bufferCount, pBindingInfos);
+		}
+		static void vkCmdSetDescriptorBufferOffsetsEXT(
+			VkCommandBuffer commandBuffer, VkPipelineBindPoint pipelineBindPoint,
+			VkPipelineLayout layout, std::uint32_t firstSet, std::uint32_t setCount,
+			const std::uint32_t* pBufferIndices, const VkDeviceSize* pOffsets
+		)
+		{
+			s_vkCmdSetDescriptorBufferOffsetsEXT(
+				commandBuffer, pipelineBindPoint, layout, firstSet, setCount, pBufferIndices, pOffsets
+			);
+		}
+		static void vkGetBufferOpaqueCaptureDescriptorDataEXT(
+			VkDevice device, const VkBufferCaptureDescriptorDataInfoEXT* pInfo, void* pData
+		)
+		{
+			s_vkGetBufferOpaqueCaptureDescriptorDataEXT(device, pInfo, pData);
+		}
+		static void vkGetDescriptorEXT(
+			VkDevice device, const VkDescriptorGetInfoEXT* pDescriptorInfo, size_t dataSize,
+			void* pDescriptor
+		)
+		{
+			s_vkGetDescriptorEXT(device, pDescriptorInfo, dataSize, pDescriptor);
+		}
+		static void vkGetDescriptorSetLayoutBindingOffsetEXT(
+			VkDevice device, VkDescriptorSetLayout layout, std::uint32_t binding, VkDeviceSize* pOffset
+		)
+		{
+			s_vkGetDescriptorSetLayoutBindingOffsetEXT(device, layout, binding, pOffset);
+		}
+		static void vkGetDescriptorSetLayoutSizeEXT(
+			VkDevice device, VkDescriptorSetLayout layout, VkDeviceSize* pLayoutSizeInBytes
+		)
+		{
+			s_vkGetDescriptorSetLayoutSizeEXT(device, layout, pLayoutSizeInBytes);
+		}
+		static void vkGetImageOpaqueCaptureDescriptorDataEXT(
+			VkDevice device, const VkImageCaptureDescriptorDataInfoEXT* pInfo, void* pData
+		)
+		{
+			s_vkGetImageOpaqueCaptureDescriptorDataEXT(device, pInfo, pData);
+		}
+		static void vkGetImageViewOpaqueCaptureDescriptorDataEXT(
+			VkDevice device, const VkImageViewCaptureDescriptorDataInfoEXT* pInfo, void* pData
+		)
+		{
+			s_vkGetImageViewOpaqueCaptureDescriptorDataEXT(device, pInfo, pData);
+		}
+		static void vkGetSamplerOpaqueCaptureDescriptorDataEXT(
+			VkDevice device, const VkSamplerCaptureDescriptorDataInfoEXT* pInfo, void* pData
+		)
+		{
+			s_vkGetSamplerOpaqueCaptureDescriptorDataEXT(device, pInfo, pData);
+		}
+		static void vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT(
+			VkDevice device, const VkAccelerationStructureCaptureDescriptorDataInfoEXT* pInfo,
+			void* pData
+		)
+		{
+			s_vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT(device, pInfo, pData);
+		}
+
+	private:
+		inline static PFN_vkCmdBindDescriptorBufferEmbeddedSamplersEXT
+			s_vkCmdBindDescriptorBufferEmbeddedSamplersEXT = nullptr;
+		inline static PFN_vkCmdBindDescriptorBuffersEXT s_vkCmdBindDescriptorBuffersEXT = nullptr;
+		inline static PFN_vkCmdSetDescriptorBufferOffsetsEXT s_vkCmdSetDescriptorBufferOffsetsEXT = nullptr;
+		inline static PFN_vkGetBufferOpaqueCaptureDescriptorDataEXT
+			s_vkGetBufferOpaqueCaptureDescriptorDataEXT = nullptr;
+		inline static PFN_vkGetDescriptorEXT s_vkGetDescriptorEXT = nullptr;
+		inline static PFN_vkGetDescriptorSetLayoutBindingOffsetEXT
+			s_vkGetDescriptorSetLayoutBindingOffsetEXT = nullptr;
+		inline static PFN_vkGetDescriptorSetLayoutSizeEXT s_vkGetDescriptorSetLayoutSizeEXT = nullptr;
+		inline static PFN_vkGetImageOpaqueCaptureDescriptorDataEXT
+			s_vkGetImageOpaqueCaptureDescriptorDataEXT = nullptr;
+		inline static PFN_vkGetImageViewOpaqueCaptureDescriptorDataEXT
+			s_vkGetImageViewOpaqueCaptureDescriptorDataEXT = nullptr;
+		inline static PFN_vkGetSamplerOpaqueCaptureDescriptorDataEXT
+			s_vkGetSamplerOpaqueCaptureDescriptorDataEXT = nullptr;
+		inline static PFN_vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT
+			s_vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT = nullptr;
 	};
 }
 #endif
--- library/src/VK/VkExtensionManager.cpp
+++ library/src/VK/VkExtensionManager.cpp
@@ -7,7 +7,8 @@ static std::array extensionNameMap
 {
 	"VK_EXT_mesh_shader",
 	"VK_KHR_swapchain",
-	"VK_EXT_memory_budget"
+	"VK_EXT_memory_budget",
+	"VK_EXT_descriptor_buffer"
 };
 
 void VkDeviceExtensionManager::AddExtension(DeviceExtension extension) noexcept
@@ -21,6 +22,9 @@ void VkDeviceExtensionManager::PopulateExtensionFunctions(VkDevice device) const
 {
 	if (m_extensions.test(static_cast<size_t>(DeviceExtension::VkExtMeshShader)))
 		PopulateVkExtMeshShader(device);
+
+	if (m_extensions.test(static_cast<size_t>(DeviceExtension::VkExtDescriptorBuffer)))
+		PopulateVkExtDescriptorBuffer(device);
 }
 
 const std::vector<const char*>& VkDeviceExtensionManager::GetExtensionNames() const noexcept
@@ -33,6 +37,7 @@ bool VkDeviceExtensionManager::IsExtensionActive(DeviceExtension extension) cons
 	return m_extensions.test(static_cast<size_t>(extension));
 }
 
+// Boring function pointer population functions
 void VkDeviceExtensionManager::PopulateVkExtMeshShader(VkDevice device) noexcept
 {
 	using namespace VkDeviceExtension;
@@ -43,5 +48,53 @@ void VkDeviceExtensionManager::PopulateVkExtMeshShader(VkDevice device) noexcept
 	);
 	PopulateFunctionPointer(
 		device, "vkCmdDrawMeshTasksIndirectEXT", VkExtMeshShader::s_vkCmdDrawMeshTasksIndirectEXT
+	);
+}
+
+void VkDeviceExtensionManager::PopulateVkExtDescriptorBuffer(VkDevice device) noexcept
+{
+	using namespace VkDeviceExtension;
+
+	PopulateFunctionPointer(
+		device, "vkCmdBindDescriptorBufferEmbeddedSamplersEXT",
+		VkExtDescriptorBuffer::s_vkCmdBindDescriptorBufferEmbeddedSamplersEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkCmdBindDescriptorBuffersEXT", VkExtDescriptorBuffer::s_vkCmdBindDescriptorBuffersEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkCmdSetDescriptorBufferOffsetsEXT",
+		VkExtDescriptorBuffer::s_vkCmdSetDescriptorBufferOffsetsEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetBufferOpaqueCaptureDescriptorDataEXT",
+		VkExtDescriptorBuffer::s_vkGetBufferOpaqueCaptureDescriptorDataEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetDescriptorEXT", VkExtDescriptorBuffer::s_vkGetDescriptorEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetDescriptorSetLayoutBindingOffsetEXT",
+		VkExtDescriptorBuffer::s_vkGetDescriptorSetLayoutBindingOffsetEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetDescriptorSetLayoutSizeEXT",
+		VkExtDescriptorBuffer::s_vkGetDescriptorSetLayoutSizeEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetImageOpaqueCaptureDescriptorDataEXT",
+		VkExtDescriptorBuffer::s_vkGetImageOpaqueCaptureDescriptorDataEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetImageViewOpaqueCaptureDescriptorDataEXT",
+		VkExtDescriptorBuffer::s_vkGetImageViewOpaqueCaptureDescriptorDataEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetSamplerOpaqueCaptureDescriptorDataEXT",
+		VkExtDescriptorBuffer::s_vkGetSamplerOpaqueCaptureDescriptorDataEXT
+	);
+	PopulateFunctionPointer(
+		device, "vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT",
+		VkExtDescriptorBuffer::s_vkGetAccelerationStructureOpaqueCaptureDescriptorDataEXT
 	);
 }
