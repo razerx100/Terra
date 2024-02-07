7c0514477d46e1279261d337963e6969d68bb643
razerx100 <saikatmandal8@gmail.com>
2024-02-07 22:25:27 +0000

    Update the syncobjects
    Added single wrapper objects for VkFence and VkSemaphore

--- library/includes/VK/VkSyncObjects.hpp
+++ library/includes/VK/VkSyncObjects.hpp
@@ -1,27 +1,200 @@
 #ifndef VK_SYNC_OBJECTS_HPP_
 #define VK_SYNC_OBJECTS_HPP_
 #include <vulkan/vulkan.hpp>
-#include <VKFence.hpp>
-#include <VKSemaphore.hpp>
+#include <vector>
+
+template<typename ObjType>
+class VkSyncObj
+{
+public:
+	VkSyncObj(VkDevice device) : m_device{ device }, m_syncObj{ VK_NULL_HANDLE } {}
+	virtual ~VkSyncObj() = default;
+
+	[[nodiscard]]
+	ObjType Get() const noexcept { return m_syncObj; }
+
+protected:
+	VkDevice m_device;
+	ObjType  m_syncObj;
+
+public:
+	VkSyncObj(const VkSyncObj&) = delete;
+	VkSyncObj& operator=(const VkSyncObj&) = delete;
+
+	VkSyncObj(VkSyncObj&& other) noexcept : m_device{ other.m_device }, m_syncObj{ other.m_syncObj }
+	{
+		other.m_syncObj = VK_NULL_HANDLE;
+	}
+	VkSyncObj& operator=(VkSyncObj&& other) noexcept
+	{
+		m_device        = other.m_device;
+		m_syncObj       = other.m_syncObj;
+		other.m_syncObj = VK_NULL_HANDLE;
+
+		return *this;
+	}
+};
+
+class VKSemaphore : public VkSyncObj<VkSemaphore>
+{
+public:
+	VKSemaphore(VkDevice device) : VkSyncObj{ device } {}
+	~VKSemaphore() noexcept;
+
+	void Create();
+
+public:
+	VKSemaphore(const VKSemaphore&) = delete;
+	VKSemaphore& operator=(const VKSemaphore&) = delete;
+
+	VKSemaphore(VKSemaphore&& other) noexcept : VkSyncObj{ std::move(other) } {}
+	VKSemaphore& operator=(VKSemaphore&& other) noexcept
+	{
+		VkSyncObj::operator=(std::move(other));
+
+		return *this;
+	}
+};
+
+class VKFence : public VkSyncObj<VkFence>
+{
+public:
+	VKFence(VkDevice device) : VkSyncObj{ device } {}
+	~VKFence() noexcept;
+
+	void Create(bool signaled);
+
+	void Wait() const;
+	void Reset() const;
+
+public:
+	VKFence(const VKFence&) = delete;
+	VKFence& operator=(const VKFence&) = delete;
+
+	VKFence(VKFence&& other) noexcept : VkSyncObj{ std::move(other) } {}
+	VKFence& operator=(VKFence&& other) noexcept
+	{
+		VkSyncObj::operator=(std::move(other));
+
+		return *this;
+	}
+};
+
+template<typename SyncType>
+class VkMultiSyncObj
+{
+public:
+	VkMultiSyncObj(VkDevice device) : m_device{ device }, m_syncObjs{}, m_currentIndex{ 0u } {}
+	virtual ~VkMultiSyncObj() = default;
+
+	void AdvanceInQueue() noexcept
+	{
+		++m_currentIndex;
+		m_currentIndex %= std::size(m_syncObjs);
+	}
+
+	[[nodiscard]]
+	auto GetFront() const noexcept { return m_syncObjs.at(m_currentIndex).Get(); }
+
+protected:
+	VkDevice              m_device;
+	std::vector<SyncType> m_syncObjs;
+	size_t                m_currentIndex;
+
+public:
+	VkMultiSyncObj(const VkMultiSyncObj&) = delete;
+	VkMultiSyncObj& operator=(const VkMultiSyncObj&) = delete;
+
+	VkMultiSyncObj(VkMultiSyncObj&& other) noexcept
+		: m_device{ other.m_device }, m_syncObjs{ std::move(other.m_syncObjs) }
+		, m_currentIndex{ other.m_currentIndex } {}
+
+	VkMultiSyncObj& operator=(VkMultiSyncObj&& other) noexcept
+	{
+		m_device       = other.m_device;
+		m_syncObjs     = std::move(other.m_syncObjs);
+		m_currentIndex = other.m_currentIndex;
+
+		return *this;
+	}
+};
+
+class VkMultiSemaphore : public VkMultiSyncObj<VKSemaphore>
+{
+public:
+	VkMultiSemaphore(VkDevice device, size_t semaphoreCount);
+
+public:
+	VkMultiSemaphore(const VkMultiSemaphore&) = delete;
+	VkMultiSemaphore& operator=(const VkMultiSemaphore&) = delete;
+
+	VkMultiSemaphore(VkMultiSemaphore&& other) noexcept : VkMultiSyncObj{ std::move(other) } {}
+	VkMultiSemaphore& operator=(VkMultiSemaphore&& other) noexcept
+	{
+		VkMultiSyncObj::operator=(std::move(other));
+
+		return *this;
+	}
+};
+
+class VkMultiFence : public VkMultiSyncObj<VKFence>
+{
+public:
+	VkMultiFence(VkDevice device, size_t fenceCount, bool signaled);
+
+	void WaitForFrontFence() const noexcept;
+	void ResetFrontFence() const noexcept;
+
+public:
+	VkMultiFence(const VkMultiFence&) = delete;
+	VkMultiFence& operator=(const VkMultiFence&) = delete;
+
+	VkMultiFence(VkMultiFence&& other) noexcept : VkMultiSyncObj{ std::move(other) } {}
+	VkMultiFence& operator=(VkMultiFence&& other) noexcept
+	{
+		VkMultiSyncObj::operator=(std::move(other));
+
+		return *this;
+	}
+};
 
 class VkSyncObjects
 {
 public:
-	VkSyncObjects(VkDevice device, std::uint32_t bufferCount = 1u, bool signaledFence = false);
+	VkSyncObjects(VkDevice device, std::uint32_t bufferCount = 1u, bool signaledFence = false)
+		: m_fences{ device, bufferCount, signaledFence }, m_semaphores{ device, bufferCount } {}
 
-	void ResetFrontFence() const noexcept;
-	void WaitForFrontFence() const noexcept;
-	void AdvanceFenceInQueue() noexcept;
-	void AdvanceSemaphoreInQueue() noexcept;
-	void AdvanceSyncObjectsInQueue() noexcept;
+	void ResetFrontFence() const noexcept { m_fences.ResetFrontFence(); }
+	void WaitForFrontFence() const noexcept { m_fences.WaitForFrontFence(); }
+	void AdvanceFenceInQueue() noexcept { m_fences.AdvanceInQueue(); }
+	void AdvanceSemaphoreInQueue() noexcept { m_semaphores.AdvanceInQueue(); }
+	void AdvanceSyncObjectsInQueue() noexcept
+	{
+		AdvanceFenceInQueue();
+		AdvanceSemaphoreInQueue();
+	}
 
 	[[nodiscard]]
-	VkFence GetFrontFence() const noexcept;
+	VkFence GetFrontFence() const noexcept { return m_fences.GetFront(); }
 	[[nodiscard]]
-	VkSemaphore GetFrontSemaphore() const noexcept;
+	VkSemaphore GetFrontSemaphore() const noexcept { return m_semaphores.GetFront(); }
 
 private:
-	VKFence m_fences;
-	VKSemaphore m_semaphores;
+	VkMultiFence     m_fences;
+	VkMultiSemaphore m_semaphores;
+
+public:
+	VkSyncObjects(const VkSyncObjects&) = delete;
+	VkSyncObjects& operator=(const VkSyncObjects&) = delete;
+
+	VkSyncObjects(VkSyncObjects&& other) noexcept
+		: m_fences{ std::move(other.m_fences) }, m_semaphores{ std::move(other.m_semaphores) } {}
+	VkSyncObjects& operator=(VkSyncObjects&& other) noexcept
+	{
+		m_fences     = std::move(other.m_fences);
+		m_semaphores = std::move(other.m_semaphores);
+
+		return *this;
+	}
 };
 #endif
--- library/src/VK/VkSyncObjects.cpp
+++ library/src/VK/VkSyncObjects.cpp
@@ -1,33 +1,85 @@
 #include <VkSyncObjects.hpp>
+#include <limits>
 
-VkSyncObjects::VkSyncObjects(VkDevice device, std::uint32_t bufferCount, bool signaledFence)
-	: m_fences{ device, bufferCount, signaledFence }, m_semaphores{ device, bufferCount } {}
-
-void VkSyncObjects::ResetFrontFence() const noexcept {
-	m_fences.ResetFrontFence();
+// VK Semaphore
+VKSemaphore::~VKSemaphore() noexcept
+{
+	vkDestroySemaphore(m_device, m_syncObj, nullptr);
 }
 
-void VkSyncObjects::WaitForFrontFence() const noexcept {
-	m_fences.WaitForFrontFence();
+void VKSemaphore::Create()
+{
+	VkSemaphoreCreateInfo semaphoreInfo{ .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+
+	vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_syncObj);
 }
 
-void VkSyncObjects::AdvanceFenceInQueue() noexcept {
-	m_fences.AdvanceInQueue();
+// VK Fence
+VKFence::~VKFence() noexcept
+{
+	vkDestroyFence(m_device, m_syncObj, nullptr);
 }
 
-void VkSyncObjects::AdvanceSemaphoreInQueue() noexcept {
-	m_semaphores.AdvanceInQueue();
+void VKFence::Create(bool signaled)
+{
+	VkFenceCreateInfo fenceInfo{
+		.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+		.flags = signaled ? VK_FENCE_CREATE_SIGNALED_BIT : 0u
+	};
+
+	vkCreateFence(m_device, &fenceInfo, nullptr, &m_syncObj);
 }
 
-void VkSyncObjects::AdvanceSyncObjectsInQueue() noexcept {
-	AdvanceFenceInQueue();
-	AdvanceSemaphoreInQueue();
+void VKFence::Wait() const
+{
+	vkWaitForFences(m_device, 1u, &m_syncObj, VK_TRUE, std::numeric_limits<std::uint64_t>::max());
 }
 
-VkFence VkSyncObjects::GetFrontFence() const noexcept {
-	return m_fences.GetFrontFence();
+void VKFence::Reset() const
+{
+	vkResetFences(m_device, 1u, &m_syncObj);
 }
 
-VkSemaphore VkSyncObjects::GetFrontSemaphore() const noexcept {
-	return m_semaphores.GetFrontSemaphore();
+// VK Multi Semaphore
+VkMultiSemaphore::VkMultiSemaphore(VkDevice device, size_t semaphoreCount) : VkMultiSyncObj{ device }
+{
+	for (size_t _ = 0u; _ < semaphoreCount; ++_)
+	{
+		VKSemaphore semaphore{ device };
+		semaphore.Create();
+		m_syncObjs.emplace_back(std::move(semaphore));
+	}
+}
+
+// VK Multi Semaphore
+VkMultiFence::VkMultiFence(VkDevice device, size_t fenceCount, bool signaled) : VkMultiSyncObj{ device }
+{
+	assert(fenceCount != 0u && "Fence Count can't be zero");
+
+	{
+		VKFence nonSignaledFence{ device };
+		nonSignaledFence.Create(false);
+		m_syncObjs.emplace_back(std::move(nonSignaledFence));
+	}
+
+	--fenceCount;
+
+	for (size_t _ = 0u; _ < fenceCount; ++_)
+	{
+		VKFence signaledFence{ device };
+		signaledFence.Create(signaled);
+		m_syncObjs.emplace_back(std::move(signaledFence));
+	}
+}
+
+void VkMultiFence::WaitForFrontFence() const noexcept
+{
+	for (const VKFence& fence : m_syncObjs)
+		fence.Wait();
+}
+
+void VkMultiFence::ResetFrontFence() const noexcept
+{
+	for (const VKFence& fence : m_syncObjs)
+		fence.Reset();
 }
