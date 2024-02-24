50394b4dd8da53a4bb916fc47ccf095d2ebc3e85
razerx100 <saikatmandal8@gmail.com>
2024-02-24 23:40:28 +0000

    Add surface to Swapchain test

--- test/src/SwapchainTest.cc
+++ test/src/SwapchainTest.cc
@@ -3,10 +3,18 @@
 
 #include <VKInstanceManager.hpp>
 #include <VkDeviceManager.hpp>
+#include <ISurfaceManager.hpp>
+#ifdef TERRA_WIN32
+#include <SurfaceManagerWin32.hpp>
+#include <SimpleWindow.hpp>
+#endif
 #include <VKRenderPass.hpp>
+#include <SwapChainManager.hpp>
 
 namespace Constants
 {
+	constexpr std::uint32_t width  = 1920u;
+	constexpr std::uint32_t height = 1080u;
 	constexpr const char* appName  = "TerraTest";
 }
 
@@ -18,6 +26,10 @@ protected:
 
 protected:
 	inline static std::unique_ptr<VkInstanceManager> s_instanceManager;
+	inline static std::unique_ptr<ISurfaceManager>   s_surfaceManager;
+#ifdef TERRA_WIN32
+	inline static std::unique_ptr<SimpleWindow>      s_window;
+#endif
 	inline static std::unique_ptr<VkDeviceManager>   s_deviceManager;
 };
 
@@ -25,21 +37,50 @@ void SwapchainTest::SetUpTestSuite()
 {
 	const CoreVersion coreVersion = CoreVersion::V1_3;
 
+#ifdef TERRA_WIN32
+	s_window         = std::make_unique<SimpleWindow>(
+		Constants::width, Constants::height, Constants::appName
+	);
+	s_surfaceManager = std::make_unique<SurfaceManagerWin32>();
+#endif
+
 	s_instanceManager = std::make_unique<VkInstanceManager>(Constants::appName);
 	s_instanceManager->DebugLayers().AddDebugCallback(DebugCallbackType::standardError);
+
+	{
+		VkInstanceExtensionManager& extensionManager = s_instanceManager->ExtensionManager();
+
+#ifdef TERRA_WIN32
+		extensionManager.AddExtensions(s_surfaceManager->GetRequiredExtensions());
+#endif
+	}
+
 	s_instanceManager->CreateInstance(coreVersion);
 
 	VkInstance vkInstance = s_instanceManager->GetVKInstance();
 
+	VkSurfaceKHR surface = VK_NULL_HANDLE;
+
+#ifdef TERRA_WIN32
+	s_surfaceManager->Create(vkInstance, s_window->GetWindowHandle(), s_window->GetModuleInstance());
+	surface = s_surfaceManager->Get();
+#endif
+
 	s_deviceManager = std::make_unique<VkDeviceManager>();
 
-	// TODO: Add a valid surface for testing. The validation error is because of the null surface.
-	s_deviceManager->FindPhysicalDevice(vkInstance, VK_NULL_HANDLE).CreateLogicalDevice(coreVersion);
+	{
+		VkDeviceExtensionManager& extensionManager = s_deviceManager->ExtensionManager();
+
+		extensionManager.AddExtensions(SwapChainManager::GetRequiredExtensions());
+	}
+
+	s_deviceManager->FindPhysicalDevice(vkInstance, surface).CreateLogicalDevice(coreVersion);
 }
 
 void SwapchainTest::TearDownTestSuite()
 {
 	s_deviceManager.reset();
+	s_surfaceManager.reset();
 	s_instanceManager.reset();
 }
 
