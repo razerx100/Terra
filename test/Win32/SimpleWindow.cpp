977ada45c110e332a214de8c8b08d2f97b990b3e
razerx100 <saikatmandal8@gmail.com>
2024-02-24 23:02:02 +0000

    Add SimpleWindow to test

---
+++ test/Win32/SimpleWindow.cpp
@@ -0,0 +1,63 @@
+#include <SimpleWindow.hpp>
+
+WindowClass::WindowClass() noexcept
+	: m_wndClass{
+		.cbSize        = static_cast<UINT>(sizeof(m_wndClass)),
+		.style         = CS_OWNDC,
+		.lpfnWndProc   = SimpleWindow::HandleMsgSetup,
+		.lpszClassName = GetName()
+	} {}
+
+WindowClass::~WindowClass() noexcept
+{
+	if (m_wndClass.hInstance)
+		UnregisterClassA(GetName(), m_wndClass.hInstance);
+}
+
+void WindowClass::Register() noexcept
+{
+	m_wndClass.hInstance = GetModuleHandleA(nullptr);
+
+	RegisterClassEx(&m_wndClass);
+}
+
+// Simple Window
+SimpleWindow::SimpleWindow(
+	std::uint32_t width, std::uint32_t height, const char* name
+) : m_hWnd{ nullptr }, m_windowClass{}
+{
+	m_windowClass.Register();
+
+	DWORD windowStyle = WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU;
+
+	RECT wr{
+		.left   = 0,
+		.top    = 0,
+		.right  = static_cast<LONG>(width),
+		.bottom = static_cast<LONG>(height)
+	};
+
+	AdjustWindowRect(&wr, windowStyle, FALSE);
+
+	m_hWnd = CreateWindowExA(
+		0,
+		m_windowClass.GetName(), name,
+		windowStyle,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		static_cast<int>(wr.right - wr.left),
+		static_cast<int>(wr.bottom - wr.top),
+		nullptr, nullptr, m_windowClass.GetHInstance(), this
+	);
+}
+
+SimpleWindow::~SimpleWindow() noexcept
+{
+	if (m_hWnd)
+		SetWindowLongPtr(m_hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(DefWindowProcA));
+}
+
+LRESULT CALLBACK SimpleWindow::HandleMsgSetup(
+	HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
+) noexcept {
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
---
+++ test/Win32/SimpleWindow.hpp
@@ -0,0 +1,78 @@
+#ifndef SIMPLE_WINDOW_HPP_
+#define SIMPLE_WINDOW_HPP_
+#include <cstdint>
+#include <utility>
+#include <CleanWin.hpp>
+
+class WindowClass {
+public:
+	WindowClass() noexcept;
+	~WindowClass() noexcept;
+
+	void Register() noexcept;
+
+	[[nodiscard]]
+	static const char* GetName() noexcept { return wndClassName; }
+
+	[[nodiscard]]
+	HINSTANCE GetHInstance() const noexcept { return m_wndClass.hInstance; }
+
+private:
+	WNDCLASSEX m_wndClass;
+
+	static constexpr const char* wndClassName = "Luna";
+
+public:
+	WindowClass(const WindowClass&) = delete;
+	WindowClass& operator=(const WindowClass&) = delete;
+
+	WindowClass(WindowClass&& other) noexcept : m_wndClass{ other.m_wndClass }
+	{
+		other.m_wndClass.hInstance = nullptr;
+	}
+	WindowClass& operator=(WindowClass&& other) noexcept
+	{
+		m_wndClass                 = other.m_wndClass;
+		other.m_wndClass.hInstance = nullptr;
+
+		return *this;
+	}
+};
+
+class SimpleWindow {
+public:
+	SimpleWindow(std::uint32_t width, std::uint32_t height, const char* name);
+	~SimpleWindow() noexcept;
+
+	[[nodiscard]]
+	void* GetWindowHandle() const noexcept { return m_hWnd; }
+	[[nodiscard]]
+	void* GetModuleInstance() const noexcept { return m_windowClass.GetHInstance(); }
+
+	static LRESULT CALLBACK HandleMsgSetup(
+		HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
+	) noexcept;
+
+private:
+	HWND        m_hWnd;
+	WindowClass m_windowClass;
+
+public:
+	SimpleWindow(const SimpleWindow&) = delete;
+	SimpleWindow& operator=(const SimpleWindow&) = delete;
+
+	SimpleWindow(SimpleWindow&& other) noexcept
+		: m_hWnd{ other.m_hWnd }, m_windowClass{ std::move(other.m_windowClass) }
+	{
+		other.m_hWnd = nullptr;
+	}
+	SimpleWindow& operator=(SimpleWindow&& other) noexcept
+	{
+		m_hWnd        = other.m_hWnd;
+		m_windowClass = std::move(other.m_windowClass);
+		other.m_hWnd  = nullptr;
+
+		return *this;
+	}
+};
+#endif
