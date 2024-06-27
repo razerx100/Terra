ecab747842845d42dcae87eda37672a35f845677
razerx100 <saikatmandal8@gmail.com>
2024-06-27 21:56:42 +0100

    Add tests for RenderEngine

---
+++ test/src/RenderEngineTest.cc
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <memory>
+
+#include <VKInstanceManager.hpp>
+#include <VkDeviceManager.hpp>
+#include <RenderEngineVertexShader.hpp>
+
+namespace Constants
+{
+	constexpr const char* appName      = "TerraTest";
+	constexpr std::uint32_t frameCount = 2u;
+	constexpr CoreVersion coreVersion  = CoreVersion::V1_3;
+}
+
+class RenderEngineTest : public ::testing::Test
+{
+protected:
+	static void SetUpTestSuite();
+	static void TearDownTestSuite();
+
+protected:
+	inline static std::unique_ptr<VkInstanceManager> s_instanceManager;
+};
+
+void RenderEngineTest::SetUpTestSuite()
+{
+	s_instanceManager = std::make_unique<VkInstanceManager>(Constants::appName);
+	s_instanceManager->DebugLayers().AddDebugCallback(DebugCallbackType::standardError);
+	s_instanceManager->CreateInstance(Constants::coreVersion);
+}
+
+void RenderEngineTest::TearDownTestSuite()
+{
+	s_instanceManager.reset();
+}
+
+TEST_F(RenderEngineTest, RenderEngineVSIndividualTest)
+{
+	VkDeviceManager deviceManager{};
+
+	{
+		VkDeviceExtensionManager& extensionManager = deviceManager.ExtensionManager();
+		RenderEngineVSIndividualDeviceExtension{}.SetDeviceExtensions(extensionManager);
+	}
+
+	{
+		VkInstance vkInstance = s_instanceManager->GetVKInstance();
+
+		deviceManager.SetDeviceFeatures(Constants::coreVersion)
+			.SetPhysicalDeviceAutomatic(vkInstance)
+			.CreateLogicalDevice();
+	}
+
+	auto threadPool = std::make_shared<ThreadPool>(2u);
+
+	RenderEngineVSIndividual renderEngine{ deviceManager, threadPool, Constants::frameCount };
+}
