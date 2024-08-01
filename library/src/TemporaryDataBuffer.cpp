478c204e21dfd51bed393e43a1ce161dc5956690
razerx100 <saikatmandal8@gmail.com>
2024-08-01 18:52:28 +0100

    Change the SetUsed logic a little
    This should make it a bit faster.

--- library/src/TemporaryDataBuffer.cpp
+++ library/src/TemporaryDataBuffer.cpp
@@ -2,9 +2,20 @@
 
 void TemporaryDataBufferGPU::SetUsed(size_t frameIndex) noexcept
 {
-	for (auto& tempBuffer : m_tempBuffers)
-		if (tempBuffer.ownerIndex == std::numeric_limits<size_t>::max())
-			tempBuffer.ownerIndex = frameIndex;
+	// The tempBuffer should be like a queue and the front of it should be already used.
+	// So, it should be a bit faster if I start marking the new resources used from the
+	// back.
+	for (auto rIt = std::rbegin(m_tempBuffers); rIt != std::rend(m_tempBuffers); ++rIt)
+	{
+		TempBuffer& tempBuffer = *rIt;
+
+		// Once we have hit the first tempBuffer which has an owner, every next buffer
+		// should also have an owner. So, we shouldn't need to go any farther.
+		if (tempBuffer.ownerIndex != std::numeric_limits<size_t>::max())
+			break;
+
+		tempBuffer.ownerIndex = frameIndex;
+	}
 }
 
 void TemporaryDataBufferGPU::Clear(size_t frameIndex) noexcept
